73c2aa81827078f9687a8c519542c4a660b40c98
simon <simoncoffey9@gmail.com>
2020-02-02 03:32:24 +0000

    correlation of three weeks prior points to following weeks points

---
+++ 3_week_model.py
@@ -0,0 +1,38 @@
+import test_api as api
+import os
+import numpy as np
+
+
+player_names = [name for name in os.listdir("./players/")]
+
+player_history_paths = ["./players/{}/history.csv".format(name) for name in player_names]
+
+print (player_history_paths[1])
+
+def player_correlation_3_weeks_prior(path_to_player_hist):
+    with open(path_to_player_hist, 'r') as file:
+        # print(len(file.readlines()))
+        lines = file.readlines()
+        three_weeks_prior= []
+        next_week = []
+        for x in range(4, len(lines)):
+            prior_three_week_total = int(lines[x-3].split(',')[3]) + \
+                                     int(lines[x-2].split(',')[3]) + \
+                                     int(lines[x-1].split(',')[3])
+            week_points = int(lines[x].split(',')[3])
+            # print("three weeks prior: {}, next week: {}".format(prior_three_week_total, week_points))
+            three_weeks_prior.append(prior_three_week_total)
+            next_week.append(week_points)
+        # print(np.corrcoef(three_weeks_prior, next_week))
+        return np.corrcoef(three_weeks_prior, next_week)[0][1]
+
+if __name__ == "__main__":
+    correlations = []
+    for player in player_history_paths:
+        corr = player_correlation_3_weeks_prior(player)
+        # print(corr)
+        if corr >= 0 or corr <= 0 :
+            correlations.append(corr)
+
+    print(np.mean(correlations))
+    print(np.std(correlations))
